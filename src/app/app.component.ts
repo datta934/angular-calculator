009884f0adba40b847ea2186aa5192bea4ecd059
mauaa <mauaa@koder.com>
2020-05-10 17:55:11 +0530

    UI changes, added paperwork css and mod function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,22 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'calculator';
-  mainText = "";
-  subText = "";
+  mainText = '';
+  subText = '';
   operand1: number;
   operand2: number;
-  operator = "";
-  calculationString = "";
+  operator = '';
+  calculationString = '';
   answered = false;
   operatorSet = false;
 
   pressKey(key: string) {
-    if (key === "/" || key === "-" || key === "+" || key === "x") {
+    if (key === '/' || key === '-' || key === '+' || key === 'x' || key === '%') {
       const lastKey = this.mainText[this.mainText.length - 1];
-      if (lastKey === "/" || lastKey === "-" || lastKey === "+" || lastKey === "x") {
+      if (lastKey === '/' || lastKey === '-' || lastKey === '+' || lastKey === 'x') {
         this.operatorSet = true;
       }
-      if ((this.operatorSet) || (this.mainText === "")) {
+      if ((this.operatorSet) || (this.mainText === '')) {
         return;
       }
       this.operand1 = parseFloat(this.mainText);
@@ -38,39 +38,43 @@ export class AppComponent {
   getAnswer() {
     this.calculationString = this.mainText;
     this.operand2 = parseFloat(this.mainText.split(this.operator)[1]);
-    if (this.operator === "/") {
+    if (this.operator === '/') {
       this.subText = this.mainText;
       this.mainText = (this.operand1 / this.operand2).toString();
       this.subText = this.calculationString;
       if (this.mainText.length > 9) {
         this.mainText = this.mainText.substr(0, 9);
       }
-    }
-    else if (this.operator === "*") {
+    } else if (this.operator === 'x') {
       this.subText = this.mainText;
       this.mainText = (this.operand1 * this.operand2).toString();
       this.subText = this.calculationString;
       if (this.mainText.length > 9) {
-        this.mainText = "ERROR"
-        this.subText = "Range exceeded";
+        this.mainText = 'ERROR';
+        this.subText = 'Range exceeded';
       }
-    }
-    else if (this.operator === "-") {
+    } else if (this.operator === '-') {
       this.subText = this.mainText;
       this.mainText = (this.operand1 - this.operand2).toString();
       this.subText = this.calculationString;
-    }
-    else if (this.operator === "+") {
+    } else if (this.operator === '+') {
       this.subText = this.mainText;
       this.mainText = (this.operand1 + this.operand2).toString();
       this.subText = this.calculationString;
       if (this.mainText.length > 9) {
-        this.mainText = "ERROR"
-        this.subText = "Range exceeded";
+        this.mainText = 'ERROR';
+        this.subText = 'Range exceeded';
       }
-    }
-    else {
-      this.subText = "Error: Invalid Operation";
+    } else if (this.operator === '%') {
+      this.subText = this.mainText;
+      this.mainText = (this.operand1 % this.operand2).toString();
+      this.subText = this.calculationString;
+      if (this.mainText.length > 9) {
+        this.mainText = 'ERROR';
+        this.subText = 'Range exceeded';
+      }
+    } else {
+      this.subText = 'Error: Invalid Operation';
     }
     this.answered = true;
   }
